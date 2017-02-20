80ea9e9638c2aeeb2346dbe1bdbabd05a49a169c
Nir Tzur <nir.tzur@samanage.com>
2017-02-21 00:54:51 +0200

    more

--- mkfi.js
+++ mkfi.js
@@ -159,26 +159,26 @@ window.addListeners = function($) {
 
   // Calculate field 'Meir' on cashflow report - monthly
     var revised = [];
-    Array.from($('#kn-report-view_474-3 tbody tr')).forEach(function(_val, index) {
-      revised[index] = parseFloat($(this).getElementsByTagName('td')[2].innerText.substring(1).replace(',', ''));
+    Array.from($('#kn-report-view_474-3 tbody tr')).forEach(function(val, index) {
+      revised[index] = parseFloat(val.getElementsByTagName('td')[2].innerText.substring(1).replace(',', ''));
     });
-    Array.from($('#kn-report-view_474-4 tbody tr')).forEach(function(_val, index) {
-      var rafi = parseFloat($(this).getElementsByTagName('td')[1].innerText.substring(1).replace(',', ''));
-      var ibds = parseFloat($(this).getElementsByTagName('td')[2].innerText.substring(1).replace(',', ''));
+    Array.from($('#kn-report-view_474-4 tbody tr')).forEach(function(val, index) {
+      var rafi = parseFloat(val.getElementsByTagName('td')[1].innerText.substring(1).replace(',', ''));
+      var ibds = parseFloat(val.getElementsByTagName('td')[2].innerText.substring(1).replace(',', ''));
       var meir = (revised[index] - rafi)/2 + ibds;
-      $(this).getElementsByTagName('td')[3].innerText = '$' + meir.to_s;
+      val.getElementsByTagName('td')[3].innerText = '$' + meir.to_s;
     });
 
   // Calculate field 'Meir' on cashflow report - yearly
     var revised = [];
-    Array.from($('#kn-report-view_482-3 tbody tr')).forEach(function(_val, index) {
-      revised[index] = parseFloat($(this).getElementsByTagName('td')[2].innerText.substring(1).replace(',', ''));
+    Array.from($('#kn-report-view_482-3 tbody tr')).forEach(function(val, index) {
+      revised[index] = parseFloat(val.getElementsByTagName('td')[2].innerText.substring(1).replace(',', ''));
     });
-    Array.from($('#kn-report-view_482-3 tbody tr')).forEach(function(_val, index) {
-      var rafi = parseFloat($(this).getElementsByTagName('td')[1].innerText.substring(1).replace(',', ''));
-      var ibds = parseFloat($(this).getElementsByTagName('td')[2].innerText.substring(1).replace(',', ''));
+    Array.from($('#kn-report-view_482-3 tbody tr')).forEach(function(val, index) {
+      var rafi = parseFloat(val.getElementsByTagName('td')[1].innerText.substring(1).replace(',', ''));
+      var ibds = parseFloat(val.getElementsByTagName('td')[2].innerText.substring(1).replace(',', ''));
       var meir = (revised[index] - rafi)/2 + ibds;
-      $(this).getElementsByTagName('td')[3].innerText = '$' + meir.to_s;
+      val.getElementsByTagName('td')[3].innerText = '$' + meir.to_s;
     });
   });
 
