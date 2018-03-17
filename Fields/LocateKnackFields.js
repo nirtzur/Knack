bb81110a5e0a4cd7d1429edb0202395db2e845a6
Nir Tzur <nir.tzur@samanage.com>
2018-03-17 16:21:04 +0200

    Add object name to search fields

--- Fields/LocateKnackFields.js
+++ Fields/LocateKnackFields.js
@@ -248,7 +248,7 @@ var LocateKnackFields = (function() {
     sorted_objects = temp.sort(order);
 
     sorted_objects.forEach(function(field) {
-      data.push({ value: field.key, text: field.name + " (" + field.key + ")" });
+      data.push({ value: field.key, text: field.name + " (" + field.key + " defined in " + field.origin.name + ")" });
     });
     return data;
   }
