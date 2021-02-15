508173e45509c6170db3e69b85cf0e6b6cf888e8
Nir Tzur <nir.tzur@samanage.com>
2021-02-15 10:36:35 +0200

    add spinners

--- alphahub.js
+++ alphahub.js
@@ -363,6 +363,8 @@ window.addListeners = function($) {
 
   // Create rehab types for property
   $(document).on('knack-form-submit.view_697', async function(event, view, record) {
+    Knack.showSpinner();
     await create_rehab_items(record);
+    Knack.hideSpinner();
   });
 }
